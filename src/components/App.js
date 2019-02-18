2c3e9dea1956fd87e9538eda33a9818e26445ebc
Cesar Lugo <cesarlugo00@hotmail.com>
2019-02-18 18:57:24 +0100

    no found page set & Router testing working

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import StreamEdit from "./streams/StreamEdit";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
+import NotFoundPage from "./NotFoundPage";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
-        <Route path="/streams/show" exact component={StreamShow} />
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     );
   }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,63 @@
 import React from "react";
-import { configure, shallow } from "enzyme";
-import App from '../components/App';
+import { configure, shallow, mount } from "enzyme";
+import App from "../components/App";
+import { MemoryRouter } from "react-router";
 
+import StreamList from "../components/streams/StreamList";
+import StreamCreate from "../components/streams/StreamCreate";
+import StreamEdit from "../components/streams/StreamEdit";
+import StreamDelete from "../components/streams/StreamDelete";
+import StreamShow from "../components/streams/StreamShow";
+import PageNotFound from "../components/NotFoundPage";
 
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-describe("App test", function() {
 
+
+describe("App test", function() {
   it("renders without crashing", function () {
     shallow(<App  />);
   });
 
 });
+
+const wrapper = path =>
+  mount(
+    <MemoryRouter initialEntries={[path]}>
+        <App />
+    </MemoryRouter>
+  );
+
+describe("Routes test", function() {
+   it("renders StreamList component on initial path /", () => {
+    const component = wrapper("/");
+    expect(component.find(StreamList)).toHaveLength(1);
+   });
+
+   it("renders StreamCreate component on path /streams/new ", () => {
+    const component = wrapper("/streams/new");
+    expect(component.find(StreamCreate)).toHaveLength(1);
+  });
+
+  it("renders StreamEdit component on path /streams/edit ", () => {
+    const component = wrapper("/streams/edit");
+    expect(component.find(StreamEdit)).toHaveLength(1);
+  });
+
+  it("renders StreamDelete component on path /streams/delete ", () => {
+    const component = wrapper("/streams/delete");
+    expect(component.find(StreamDelete)).toHaveLength(1);
+  });
+
+  it("renders StreamShow component on path /streams/show ", () => {
+    const component = wrapper("/streams/show");
+    expect(component.find(StreamShow)).toHaveLength(1);
+  });
+
+  it("renders PageNotFound component on path /random ", () => {
+    const component = wrapper("/random");
+    expect(component.find(PageNotFound)).toHaveLength(1);
+  });
+
+ });
